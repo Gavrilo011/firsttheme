468349da1295b0615146a19e6d219f7617c27058
Dule <baxuz011@gmail.com>
2019-12-21 22:52:19 +0100

    Cleaning Files & Serializing Tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,6 +5,8 @@ import sass from 'gulp-sass';
 import cleanCSS from 'gulp-clean-css';
 import gulpif from 'gulp-if';
 import sourcemaps from 'gulp-sourcemaps';
+import imagemin  from 'gulp-imagemin';
+import del from 'del';
 
 const PRODUCTION = yargs.argv.prod;
 
@@ -12,8 +14,18 @@ const paths = {
     styles: {
         src: ['src/assets/scss/bundle.scss', 'src/assets/scss/admin.scss'],
         dest: 'dist/assets/css'
+    },
+    images: {
+        src: 'src/assets/images/**/*.{jpg,jpeg,png,svg,gif}',
+        dest: 'dist/assets/images'
+    },
+    other: {
+        src: ['src/assets/**/*', '!src/assets/{images,js,scss}', '!src/assets/{images,js,scss}/**/*'],
+        dest: 'dist/assets'
     }
 }
+
+export const clean = () =>  del(['dist']);
 
 export const styles = () => {
     return gulp.src(paths.styles.src)
@@ -24,8 +36,24 @@ export const styles = () => {
     .pipe(gulp.dest(paths.styles.dest))
 }
 
-export const watch = () => {
-    gulp.watch('src/assets/scss/**/*.scss', styles);
+export const images = () => {
+    return gulp.src(paths.images.src)
+        .pipe(gulpif(PRODUCTION, imagemin()))
+        .pipe(gulp.dest(paths.images.dest));
 }
 
-// export default hello;
+export const watch = () => {
+    gulp.watch('src/assets/scss/**/*.scss', styles);
+    gulp.watch(paths.images.src, images);
+    gulp.watch(paths.other.src, copy);
+}
+
+export const copy = () => {
+    return gulp.src(paths.other.src)
+        .pipe(gulp.dest(paths.other.dest));
+}
+
+export const dev = gulp.series(clean, gulp.parallel(styles, images, copy), watch);
+export const build = gulp.series(clean, gulp.parallel(styles, images, copy));
+
+export default dev;
