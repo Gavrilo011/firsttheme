2b2901f58629f93aea7cfbd6904477d4894e2039
Dule <baxuz011@gmail.com>
2019-12-22 02:27:03 +0100

    Creating A Gulp Task For JavaScript

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,6 +7,8 @@ import gulpif from 'gulp-if';
 import sourcemaps from 'gulp-sourcemaps';
 import imagemin  from 'gulp-imagemin';
 import del from 'del';
+import webpack from 'webpack-stream';
+import uglify  from 'gulp-uglify';
 
 const PRODUCTION = yargs.argv.prod;
 
@@ -18,6 +20,10 @@ const paths = {
     images: {
         src: 'src/assets/images/**/*.{jpg,jpeg,png,svg,gif}',
         dest: 'dist/assets/images'
+    },
+    scrips: {
+        src: 'src/assets/js/bundle.js',
+        dest: 'dist/assets/js'
     },
     other: {
         src: ['src/assets/**/*', '!src/assets/{images,js,scss}', '!src/assets/{images,js,scss}/**/*'],
@@ -51,6 +57,32 @@ export const watch = () => {
 export const copy = () => {
     return gulp.src(paths.other.src)
         .pipe(gulp.dest(paths.other.dest));
+}
+
+export const scripts = () => {
+    return gulp.src(paths.scrips.src)
+        .pipe(webpack({
+            mode: 'development',
+            module: {
+                rules: [
+                    {
+                        test: /\.js$/,
+                        use: {
+                            loader: 'babel-loader',
+                            options: {
+                                presets: ['@babel/preset-env']
+                            }
+                        }
+                    }
+                ]
+            },
+            output: {
+                filename: 'bundle.js'
+            },
+            devtool: !PRODUCTION ? 'inline-source-map' : false,
+        }))
+        .pipe(gulpif(PRODUCTION, uglify()))
+        .pipe(gulp.dest(paths.scrips.dest));
 }
 
 export const dev = gulp.series(clean, gulp.parallel(styles, images, copy), watch);
--- lib/enqueue-assets.php
+++ lib/enqueue-assets.php
@@ -1,7 +1,9 @@
 <?php 
 
 function firsttheme_assets() {
-    wp_enqueue_style('firsttheme-stylesheet', get_template_directory_uri() . '/dist/assets/css/bundle.css' , array(), '1.0.0', 'all');
+    wp_enqueue_style('firsttheme-stylesheet', get_template_directory_uri() . '/dist/assets/css/bundle.css', array(), '1.0.0', 'all');
+
+    wp_enqueue_script('firsttheme-scrips', get_template_directory_uri() . '/dist/assets/js/bundle.js', array(), true);
 }
 
 add_action('wp_enqueue_scripts', 'firsttheme_assets');
