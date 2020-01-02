72aa3ccec00d7ccfe1a088ab37cd93d69d0193be
Dule <baxuz011@gmail.com>
2020-01-02 13:49:02 +0100

    Adding The Markup For Menu Accessibility

--- lib/navigation.php
+++ lib/navigation.php
@@ -1,5 +1,6 @@
 <?php 
 
+// Registrujem menu
 function _themename_register_menu() {
     register_nav_menus( array(
         'main-menu' => esc_html__('Main Menu', '_themename'),
@@ -12,19 +13,53 @@ function _themename_register_menu() {
 // Dokumentacija kaze da je ovo pravilnije i ako obe rade
 add_action('after_setup_theme', '_themename_register_menu');
 
+
+function _themename_aria_hasdropdown($atts, $item, $args) {
+    if($args->theme_location == 'main-menu') {
+        if(in_array('menu-item-has-children', $item->classes)) {
+            $atts['aria-haspopup'] = 'true';
+            $atts['aria-expanded'] = 'false';
+        }
+    }
+    return $atts;
+}
+add_filter( 'nav_menu_link_attributes', '_themename_aria_hasdropdown', 10, 3 );
+
+
+// Dodajem strelice na menu
+function _themename_submenu_button($dir = 'down', $title) {
+    $button = '<button class="menu-button">';
+    $button .= '<span class="u-screen-reader-text menu-button-show">' . sprintf(esc_html__('Show %s submenu', '_themename'), $title) . '</span>';
+    $button .= '<span aria-hidden="true" class="u-screen-reader-text menu-button-hide">' . sprintf(esc_html__('Hide %s submenu', '_themename'), $title) . '</span>';
+    $button .= '<i class="fa fa-angle-' . $dir . '" aria-hidden="true"></i>';
+    $button .= '</button>';
+    return $button;
+}
+
 function _themename_dropdown_icon($title, $item, $args, $depth) {
     if($args->theme_location == 'main-menu') {
         if(in_array('menu-item-has-children', $item->classes)) {
             if($depth == 0) {
-                $title .= '<i class="fa fa-angle-down" aria-hidden="true"></i>';
+                $title .= _themename_submenu_button('down', $title);
             } else {
-                $title .= '<i class="fa fa-angle-right" aria-hidden="true"></i>';
+                $title .= _themename_submenu_button('right', $title);
             }
         }
     }
     return $title;
 }
+add_filter('nav_menu_item_title', '_themename_dropdown_icon', 10, 4);
 
-add_filter('nav_menu_item_title', '_themename_dropdown_icon', 10, 4)
+
+// Funkcija da dodam moju custom klasu na menu-item
+// function _themename_add_custom_item_class( $classes, $item, $args, $depth ) {
+//     if( 'main-menu' === $args->theme_location ) {
+//         if($depth == 0) {
+//             $classes[] = "moja-klasa";
+//         }
+//     }
+//     return $classes;
+// }
+// add_filter( 'nav_menu_css_class' , '_themename_add_custom_item_class' , 10, 4 );
 
 ?>
--- searchform.php
+++ searchform.php
@@ -4,4 +4,4 @@
         <input type="search" class="c-search-form__field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', '_themename' ) ?>" value="<?php echo esc_attr(get_search_query()) ?>" name="s" />
     </label>
     <button class="c-search-form__button" type="submit"><span class="u-screen-reader-text"><?php echo esc_html_x( 'Search', 'submit button', '_themename' ); ?></span><i class="fas fa-search" aria-hidden="true"></i></button>
-</form>
+</form> 
