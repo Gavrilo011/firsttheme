91608cbddb4e40930fb41691945e117a18c0d2fc
Dule <baxuz011@gmail.com>
2020-01-08 21:48:08 +0100

    Using postMessage For Better Customizer Updates

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
         </div>
 
-        <?php get_template_part('template-parts/footer/widgets'); ?>
-        <?php get_template_part('template-parts/footer/info'); ?>
+            <?php get_template_part('template-parts/footer/widgets'); ?>
+            <?php get_template_part('template-parts/footer/info'); ?>
         
         <?php wp_footer(); ?>
     </body>
--- lib/enqueue-assets.php
+++ lib/enqueue-assets.php
@@ -8,6 +8,7 @@ function _themename_assets() {
 
 add_action( 'wp_enqueue_scripts', '_themename_assets' );
 
+
 function _themename_admin_assets() {
     wp_enqueue_style( '_themename-admin-stylesheet', get_template_directory_uri() . '/dist/assets/css/admin.css' , array(), '1.0.0', 'all' );
 
@@ -16,4 +17,11 @@ function _themename_admin_assets() {
 
 add_action('admin_enqueue_scripts', '_themename_admin_assets');
 
+
+function _themename_customize_preview_js() {
+	wp_enqueue_script( '_themename-cutomize-preview', get_template_directory_uri() . '/dist/assets/js/customize-preview.js', array('customize-preview', 'jquery'), '1.0.0' , true );
+}
+
+add_action('customize_preview_init','_themename_customize_preview_js');
+    
 ?>
